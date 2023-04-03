f422e7c717487934e4d582ada2508e858776b500
P Sudip Kumar Prusty <sudipprustysudip1905@gmail.com>
2023-04-04 00:14:27 +0530

    Login.java created.

--- ChatApplication/app/src/main/java/com/example/chatapplication/MainActivity.java
+++ ChatApplication/app/src/main/java/com/example/chatapplication/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.chatapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() = null){
+            Intent intent = new Intent(MainActivity.this, login.class);
+            startActivity(intent);
+        }
+
     }
 }
