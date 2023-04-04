b5b10c87f876ce3e69ea7a7b161d815b8dcc85eb
DustySameer <sameer.dehury007@gmail.com>
2023-04-04 07:58:29 +0530

    Update msgModelclass.java
    Some getter & setter methods were added

--- ChatApplication/app/src/main/java/com/example/chatapplication/msgModelclass.java
+++ ChatApplication/app/src/main/java/com/example/chatapplication/msgModelclass.java
@@ -1,4 +1,32 @@
 package com.example.chatapplication;
 
 public class msgModelclass {
+
+    String messsage;
+    String Senderid;
+    long timeStamp;
+
+    public String getMesssage() {
+        return messsage;
+    }
+
+    public void setMesssage(String messsage) {
+        this.messsage = messsage;
+    }
+
+    public String getSenderid() {
+        return Senderid;
+    }
+
+    public void setSenderid(String senderid) {
+        Senderid = senderid;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
 }
