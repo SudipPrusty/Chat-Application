733e0de46aa96d0afa6fb3cab8cc28256a48c6b8
God-Sensei <201801270001@cutmap.ac.in>
2023-04-09 13:15:48 +0530

    login completed

--- ChatApplication/app/src/main/java/com/example/chatapplication/login.java
+++ ChatApplication/app/src/main/java/com/example/chatapplication/login.java
@@ -3,10 +3,13 @@ package com.example.chatapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -15,8 +18,9 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
     Button button;
-    EditText email,password;
+    EditText email, password;
     FirebaseAuth auth;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,26 +28,49 @@ public class login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
 
-            auth = FirebaseAuth.getInstance();
-            button= findViewById(R.id.Logbutton);
-            email = findViewById(R.id.editTextLogEmail);
-            password = findViewById(R.id.editTextLogPassword);
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.Logbutton);
+        email = findViewById(R.id.editTextLogEmail);
+        password = findViewById(R.id.editTextLogPassword);
 
 
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    String Email = email.getText().toString();
-                    String pass = password.getText().toString();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Email = email.getText().toString();
+                String pass = password.getText().toString();
 
-                    auth.signInWithEmailAndPassword(Email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                if ((TextUtils.isEmpty(Email))) {
+                    Toast.makeText(login.this, "Enter the EMAIL", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(pass)) {
+                    Toast.makeText(login.this, "Enter the password", Toast.LENGTH_SHORT).show();
+                } else if (!Email.matches(emailPattern)) {
+                    email.setError("Give Proper Email Address");
+                } else if (password.length() < 6) {
+                    password.setError("More Than Six Characters");
+                    Toast.makeText(login.this, "Password Needs To Be Six Characters", Toast.LENGTH_SHORT).show();
+                } else {
+                    auth.signInWithEmailAndPassword(Email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-
+                            if (task.isSuccessful()) {
+                                try {
+                                    Intent intent = new Intent(login.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } catch (Exception e) {
+                                    Toast.makeText(login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    })
+                    });
 
                 }
-            });
+
+
+            }
+        });
     }
 }
