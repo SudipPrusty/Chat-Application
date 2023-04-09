b7c4e1cf1adec4c206af0826c0663ba802726d74
God-Sensei <201801270001@cutmap.ac.in>
2023-04-09 12:52:05 +0530

    login class update.1

--- ChatApplication/app/src/main/java/com/example/chatapplication/login.java
+++ ChatApplication/app/src/main/java/com/example/chatapplication/login.java
@@ -1,15 +1,49 @@
 package com.example.chatapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
+    Button button;
+    EditText email,password;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+
+            auth = FirebaseAuth.getInstance();
+            button= findViewById(R.id.Logbutton);
+            email = findViewById(R.id.editTextLogEmail);
+            password = findViewById(R.id.editTextLogPassword);
+
+
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String Email = email.getText().toString();
+                    String pass = password.getText().toString();
+
+                    auth.signInWithEmailAndPassword(Email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        }
+                    })
+
+                }
+            });
     }
 }
