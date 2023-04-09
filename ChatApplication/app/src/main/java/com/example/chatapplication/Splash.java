673429d20839544487f69001b77c696fc7d78adb
P Sudip Kumar Prusty <sudipprustysudip1905@gmail.com>
2023-04-09 14:20:28 +0530

    Animation for splash screen created.

--- ChatApplication/app/src/main/java/com/example/chatapplication/Splash.java
+++ ChatApplication/app/src/main/java/com/example/chatapplication/Splash.java
@@ -5,14 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
+
+    ImageView logo;
+    TextView name, own1;
+    Animation topAnim, bottomAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
+        logo = findViewById(R.id.logoimg);
+        name = findViewById(R.id.logonameimg);
+        own1 = findViewById(R.id.ownone);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        logo.setAnimation(topAnim);
+        name.setAnimation(topAnim);
+        own1.setAnimation(bottomAnim);
 
         new Handler().postDelayed(new Runnable() {
             @Override
