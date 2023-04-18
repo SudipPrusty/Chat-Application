b9e12c9c68048832891d105258ff20f5626dc2e6
Chiranjeeb Garnayak <garnayakchiranjeeb@gmail.com>
2023-04-19 01:33:41 +0530

    chat window activity varriables

--- ChatApplication/app/src/main/java/com/example/chatapplication/chatwindow.java
+++ ChatApplication/app/src/main/java/com/example/chatapplication/chatwindow.java
@@ -1,11 +1,22 @@
 package com.example.chatapplication;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class chatwindow extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
 
+public class chatwindow extends AppCompatActivity {
+    String reciverimg, reciverUid,reciverName,SenderUID;
+    CircleImageView profile;
+    TextView reciverNName;
+    FirebaseDatabase database;
+    FirebaseAuth firebaseAuth;
+    public  static String senderImg;
+    public  static String reciverIImg;
+    CardView sendbtn;
+    EditText textmsg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
