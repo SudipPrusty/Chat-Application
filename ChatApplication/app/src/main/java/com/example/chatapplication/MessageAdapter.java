5bc4a19df0ebfa08c1754694c88ebacba5e25c45
DustySameer <sameer.dehury007@gmail.com>
2023-04-18 23:18:49 +0530

    UPDATE
    dependencies was modified
    sender & receiver layout were created

--- ChatApplication/app/src/main/java/com/example/chatapplication/MessageAdapter.java
+++ ChatApplication/app/src/main/java/com/example/chatapplication/MessageAdapter.java
@@ -1,40 +1,94 @@
 package com.example.chatapplication;
 
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.view.menu.MenuView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class MessageAdapter extends RecyclerView.Adapter {
+
+    Context context;
+    ArrayList<msgModelclass> messageAdpaterArrayList;
+    int ITEM_SEND=1;
+    int ITEM_RECEIVE=2;
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        if(viewType == ITEM_SEND) {
+            View view = LayoutInflater.from(context).inflate(R.layout.sender_layout, parent, false);
+            return new senderViewHolder(view);
+        }
+        else{
+            View view = LayoutInflater.from(context).inflate(R.layout.receiver_layout,parent, false);
+            return new receiverviewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position)
+    {
+        msgModelclass messages = messageAdpaterArrayList.get(position);
+        if(holder.getClass()==senderViewHolder.class)
+        {
+            senderViewHolder viewholder = (senderViewHolder) holder;
+            viewholder.msgtxt.setText(messages.getMesssage());
 
+        }
+        else{
+            receiverviewHolder viewHolder = (receiverviewHolder) holder;
+            viewHolder.msgtxt.setText(messages.getMesssage());
+        }
     }
 
     @Override
     public int getItemCount() {
+
         return 0;
     }
 
-
+    @Override
+    public int getItemViewType(int position) {
+        msgModelclass messages = messageAdpaterArrayList.get(position);
+        if(FirebaseAuth.getInstance().getCurrentUser().getUid().equals(messages.getSenderid())){
+            return ITEM_SEND;
+        }
+        else {
+            return ITEM_RECEIVE;
+        }
+    }
 
     class senderViewHolder extends RecyclerView.ViewHolder {
+        CircleImageView circleImageView;
+        TextView msgtxt;
         public senderViewHolder(@NonNull View itemView) {
+
             super(itemView);
+            circleImageView = itemView.findViewById(R.id.profilerggg);
+            msgtxt = itemView.findViewById(R.id.msgsendertyp);
         }
     }
 
     class receiverviewHolder extends RecyclerView.ViewHolder {
+        CircleImageView circleImageView;
+        TextView msgtxt;
         public receiverviewHolder(@NonNull View itemView) {
+
             super(itemView);
+            circleImageView = itemView.findViewById(R.id.pro);
+            msgtxt = itemView.findViewById(R.id.recivertextset);
         }
     }
 
