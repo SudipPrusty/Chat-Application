4608783fa620724c5d8220750b2e208b5dc37d3e
Chiranjeeb Garnayak <garnayakchiranjeeb@gmail.com>
2023-04-19 00:41:39 +0530

    Update 2

---
+++ ChatApplication/app/src/main/java/com/example/chatapplication/chatwindow.java
@@ -0,0 +1,14 @@
+package com.example.chatapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class chatwindow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatwindow);
+    }
+}
