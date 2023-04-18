85d35624b465a79c7e440acb49ee248382af9539
Chiranjeeb Garnayak <garnayakchiranjeeb@gmail.com>
2023-04-18 21:13:25 +0530

    Update registration.java

--- ChatApplication/app/src/main/java/com/example/chatapplication/registration.java
+++ ChatApplication/app/src/main/java/com/example/chatapplication/registration.java
@@ -1,9 +1,6 @@
 package com.example.chatapplication;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -13,6 +10,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -28,9 +29,10 @@ import com.google.firebase.storage.UploadTask;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 
+
 public class registration extends AppCompatActivity {
     TextView loginbut;
-    EditText rg_username, rg_email, rg_password, rg_repassword;
+    EditText rg_username, rg_email , rg_password, rg_repassword;
     Button rg_signup;
     CircleImageView rg_profileImg;
     FirebaseAuth auth;
@@ -39,12 +41,15 @@ public class registration extends AppCompatActivity {
     String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
     FirebaseDatabase database;
     FirebaseStorage storage;
-
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Establishing The Account");
+        progressDialog.setCancelable(false);
         getSupportActionBar().hide();
         database = FirebaseDatabase.getInstance();
         storage = FirebaseStorage.getInstance();
@@ -56,6 +61,7 @@ public class registration extends AppCompatActivity {
         rg_repassword = findViewById(R.id.rgrepassword);
         rg_profileImg = findViewById(R.id.profilerg0);
         rg_signup = findViewById(R.id.signupbutton);
+
 
         loginbut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,31 +79,35 @@ public class registration extends AppCompatActivity {
                 String emaill = rg_email.getText().toString();
                 String Password = rg_password.getText().toString();
                 String cPassword = rg_repassword.getText().toString();
-                String status = "hey i'm using this application";
+                String status = "Hey I'm Using This Application";
 
-                if (TextUtils.isEmpty(namee) || TextUtils.isEmpty(emaill)||
-                        TextUtils.isEmpty(Password)||TextUtils.isEmpty(cPassword)){
+                if (TextUtils.isEmpty(namee) || TextUtils.isEmpty(emaill) ||
+                        TextUtils.isEmpty(Password) || TextUtils.isEmpty(cPassword)){
+                    progressDialog.dismiss();
                     Toast.makeText(registration.this, "Please Enter Valid Information", Toast.LENGTH_SHORT).show();
-                }else if (!emaill.matches(emailPattern)){
-                    rg_email.setError("Type a Valid Email Here");
+                }else  if (!emaill.matches(emailPattern)){
+                    progressDialog.dismiss();
+                    rg_email.setError("Type A Valid Email Here");
                 }else if (Password.length()<6){
-                    rg_password.setError("Password Must Be Six Characters Or More");
+                    progressDialog.dismiss();
+                    rg_password.setError("Password Must Be 6 Characters Or More");
                 }else if (!Password.equals(cPassword)){
-                    rg_repassword.setError("Password Doesn't Match");
+                    progressDialog.dismiss();
+                    rg_password.setError("The Password Doesn't Match");
                 }else {
                     auth.createUserWithEmailAndPassword(emaill,Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                String id= task.getResult().getUser().getUid();
-                                DatabaseReference reference = database.getReference().child("user").child("id");
-                                StorageReference storageReference = storage.getReference().child("Upload").child("id");
+                            if (task.isSuccessful()){
+                                String id = task.getResult().getUser().getUid();
+                                DatabaseReference reference = database.getReference().child("user").child(id);
+                                StorageReference storageReference = storage.getReference().child("Upload").child(id);
 
-                                if(imageURI!=null){
+                                if (imageURI!=null){
                                     storageReference.putFile(imageURI).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                                         @Override
                                         public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
-                                            if(task.isSuccessful()){
+                                            if (task.isSuccessful()){
                                                 storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                                                     @Override
                                                     public void onSuccess(Uri uri) {
@@ -106,14 +116,14 @@ public class registration extends AppCompatActivity {
                                                         reference.setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
                                                             @Override
                                                             public void onComplete(@NonNull Task<Void> task) {
-                                                                if(task.isSuccessful()){
+                                                                if (task.isSuccessful()){
+                                                                    progressDialog.show();
                                                                     Intent intent = new Intent(registration.this,MainActivity.class);
                                                                     startActivity(intent);
                                                                     finish();
                                                                 }else {
-                                                                    Toast.makeText(registration.this, "Error In creating the user", Toast.LENGTH_SHORT).show();
+                                                                    Toast.makeText(registration.this, "Error in creating the user", Toast.LENGTH_SHORT).show();
                                                                 }
-
                                                             }
                                                         });
                                                     }
@@ -121,29 +131,31 @@ public class registration extends AppCompatActivity {
                                             }
                                         }
                                     });
-                                }else{
-                                    String status= "Hey I'm using this application";
-                                    imageuri = "https://firebasestorage.googleapis.com/v0/b/chat-application-74201.appspot.com/o/man.png?alt=media&token=d83a924e-1442-48f2-a431-954e4e909bef";
+                                }else {
+                                    String status = "Hey I'm Using This Application";
+                                    imageuri = "https://firebasestorage.googleapis.com/v0/b/av-messenger-dc8f3.appspot.com/o/man.png?alt=media&token=880f431d-9344-45e7-afe4-c2cafe8a5257";
                                     Users users = new Users(id,namee,emaill,Password,imageuri,status);
                                     reference.setValue(users).addOnCompleteListener(new OnCompleteListener<Void>() {
                                         @Override
                                         public void onComplete(@NonNull Task<Void> task) {
-                                            if(task.isSuccessful()){
+                                            if (task.isSuccessful()){
+                                                progressDialog.show();
                                                 Intent intent = new Intent(registration.this,MainActivity.class);
                                                 startActivity(intent);
                                                 finish();
                                             }else {
-                                                Toast.makeText(registration.this, "Error In creating the user", Toast.LENGTH_SHORT).show();
+                                                Toast.makeText(registration.this, "Error in creating the user", Toast.LENGTH_SHORT).show();
                                             }
                                         }
                                     });
                                 }
-                            }else{
+                            }else {
                                 Toast.makeText(registration.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
                 }
+
             }
         });
 
@@ -154,7 +166,7 @@ public class registration extends AppCompatActivity {
                 Intent intent = new Intent();
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent,"Select a picture"),10);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"),10);
             }
         });
     }
@@ -162,9 +174,9 @@ public class registration extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode ==10){
-            if(data!=null){
-                imageURI= data.getData();
+        if (requestCode==10){
+            if (data!=null){
+                imageURI = data.getData();
                 rg_profileImg.setImageURI(imageURI);
             }
         }
